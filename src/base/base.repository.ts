04e2755bc50279b5c26c1233517aa8d8b896f359
ewjee <ewjee@ewjee-Macbook.local>
2025-02-25 09:13:52 +0900

    ✨ Add optional clientSession argument to baseRepository

--- src/base/base.repository.ts
+++ src/base/base.repository.ts
@@ -1,11 +1,13 @@
+// base.repository.ts
 import { Injectable } from '@nestjs/common';
-import { Model, UpdateQuery } from 'mongoose';
+import { Model, UpdateQuery, ClientSession } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
+
 @Injectable()
 export abstract class BaseRepository<T extends Document, DTO> {
   constructor(
-    @InjectModel((this! as typeof BaseRepository).modelName) 
+    @InjectModel((this! as typeof BaseRepository).modelName)
     protected readonly model: Model<T>
   ) {}
 
@@ -13,28 +15,30 @@ export abstract class BaseRepository<T extends Document, DTO> {
     throw new Error(`Model name not implemented for ${this.name}`);
   }
 
-  async create(dto: DTO): Promise<T> {
+  async create(dto: DTO, session: ClientSession | null = null): Promise<T> {
     try {
       const newEntity = new this.model(dto);
-      return await newEntity.save();
+      return await newEntity.save({ session }); // session is now ClientSession | null
     } catch (error) {
       console.error(`Error creating entity:`, error);
       throw new Error('Failed to create entity');
     }
   }
 
-  async findOne(id: string | number): Promise<T | null> {
+  async findOne(id: string | number, session: ClientSession | null = null): Promise<T | null> {
     try {
-      return await this.model.findById(id).exec();
+      return await this.model.findById(id).session(session).exec(); // session is ClientSession | null
     } catch (error) {
       console.error(`Error finding entity:`, error);
       throw new Error('Failed to find entity');
     }
   }
 
-  async update(id: string | number, dto: Partial<DTO>): Promise<T> {
+  async update(id: string | number, dto: Partial<DTO>, session: ClientSession | null = null): Promise<T> {
     try {
-      const updatedEntity = await this.model.findByIdAndUpdate(id, dto as UpdateQuery<T>, { new: true }).exec();
+      const updatedEntity = await this.model
+        .findByIdAndUpdate(id, dto as UpdateQuery<T>, { new: true, session }) // session is ClientSession | null
+        .exec();
       if (!updatedEntity) {
         throw new Error('Entity not found');
       }
@@ -44,6 +48,4 @@ export abstract class BaseRepository<T extends Document, DTO> {
       throw new Error('Failed to update entity');
     }
   }
-
-  // Add more common methods as needed
 }
